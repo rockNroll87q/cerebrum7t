7d34e18d36f11af0418397042a3b19f627057b5f
rockNroll <michele.svanera@glasgow.ac.uk>
2021-11-14 16:19:46 +0000

    start demo

---
+++ test.py
@@ -0,0 +1,32 @@
+#import streamlit as st
+
+#file = st.file_uploader("Pick a file")
+
+#import streamlit as st
+#x = st.slider('x')  
+#st.write(x, 'squared is', x * x)
+
+
+import streamlit as st
+import time
+import numpy as np
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
